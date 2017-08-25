f25becdf110fcc993f3ea619adf8579bbf19af7f
Michael Minges <mminges@ecsteam.com>
2017-08-25 16:49:25 -0600

    adding usernames to credential stub file. Made set-pipeline scripts executable

--- set-pipelines.sh
+++ set-pipelines.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./set-pipeline.bat lite "./ci/pipeline.yml" "./ci/env/azure-pipeline-params.yml" "./credentials.yml" azure-jumpbox
+./set-pipeline.sh lite "./ci/pipeline.yml" "./ci/env/azure-pipeline-params.yml" "./credentials.yml" azure-jumpbox
