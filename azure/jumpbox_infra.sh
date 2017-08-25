03dfcb6f00d73c2d335890112ad58b3f34358b21
Michael Minges <mminges@ecsteam.com>
2017-08-25 12:57:17 -0600

    Refactored jumpbox_infra script to hand creation of the service principle

--- azure/jumpbox_infra.sh
+++ azure/jumpbox_infra.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
 
-set -x 
-
 function create_env () {
+  # Set account level environment variables
   SUBSCRIPTION_ID=$(az account list | jq -r '.[] | select(.isDefault == true) | .id')
   TENANT_ID=$(az account list | jq -r '.[] | select(.isDefault == true) | .tenantId')
-  AAD_CLIENT_SECRET=$(cat $TERRAFORM_VARS_FILE | grep -i "client_secret" | awk '{print $3}' | tr -d '"')
 
-  # Create Azure Active Directory app
-  echo "Creating Azure AD app"
-  az ad app create --display-name "Terraform Jumpbox Service Principle" --password "$AAD_CLIENT_SECRET" --homepage "http://TerraformJumpboxAzureCPI" --identifier-uris "http://TerraformJumpboxAzureCPI"
-  APPLICATION_ID=`az ad app list | jq -r '.[] | select(.displayName == "Terraform Jumpbox Service Principle") | .appId'`
-
-  # Create a Service Principle for AAD app
-  echo "Creating a Service Principle for AAD app" 
-  az ad sp create --id $APPLICATION_ID
-
-  # Assign the Contributor role to the Service Principle
-  # Loop through until the role is assigned successfully. May take a few tries.
-  echo "Assigning Contributor role to the Service Principle"
-  az role assignment create --assignee "$APPLICATION_ID" --role "Contributor" --scope /subscriptions/$SUBSCRIPTION_ID
-  RETURN_CODE=$?
-  until [ "$RETURN_CODE" -eq "0" ]; do
-    echo "Assigning Contributor failed. Retrying in 5 seconds."
-    sleep 5
-    az role assignment create --assignee "$APPLICATION_ID" --role "Contributor" --scope /subscriptions/$SUBSCRIPTION_ID
-    RETURN_CODE=$?
-  done
+  # Create Service Principle and assign Contributor role
+  JUMPBOX_IDENTITY_AD=$(az ad sp create-for-rbac --name "http://TerraformJumpboxAzureCPI" --role="Contributor" --scopes="/subscriptions/$SUBSCRIPTION_ID" | jq -r '.| "\(.appId):\(.password)"')
+  APPLICATION_ID=$(echo $JUMPBOX_IDENTITY_AD | cut -d ':' -f1)
+  CLIENT_SECRET=$(echo $JUMPBOX_IDENTITY_AD | cut -d ':' -f2)
 
   # Create Jumpbox SSH Keypair
   ssh-keygen -q -N '' -t rsa -f ~/.ssh/azure-jumpbox -C ubuntu
@@ -37,18 +19,19 @@ function create_env () {
   echo "subscription_id                   = \"$SUBSCRIPTION_ID\"" >> $TERRAFORM_VARS_FILE
   echo "tenant_id                         = \"$TENANT_ID\"" >> $TERRAFORM_VARS_FILE
   echo "client_id                         = \"$APPLICATION_ID\"" >> $TERRAFORM_VARS_FILE
+  echo "client_secret                     = \"$CLIENT_SECRET\"" >> $TERRAFORM_VARS_FILE
   echo "vm_admin_public_key               = \"$JUMPBOX_PUBLIC_KEY\"" >> $TERRAFORM_VARS_FILE
 
   # Terraform Apply
-  echo "Running terraform plan"
-  terraform plan -var-file=$TERRAFORM_VARS_FILE
-  RETURN_CODE=$?
-  until [ $RETURN_CODE -eq "0" ]; do
-    echo "Contributor role not applied yet. Retrying!"
-    sleep 5
-    terraform plan -var-file=$TERRAFORM_VARS_FILE
-    RETURN_CODE=$?
-  done
+  echo "Running terraform apply"
+#  terraform plan -var-file=$TERRAFORM_VARS_FILE
+#  RETURN_CODE=$?
+#  until [ $RETURN_CODE -eq "0" ]; do
+#    echo "Contributor role not applied yet. Retrying!"
+#    sleep 5
+#    terraform plan -var-file=$TERRAFORM_VARS_FILE
+#    RETURN_CODE=$?
+#  done
   terraform apply -var-file=$TERRAFORM_VARS_FILE
 }
 
@@ -56,17 +39,17 @@ function destroy_env () {
   # Destroy terraformed jumpbox env 
   echo "Running terraform destroy"
   terraform destroy -var-file=$TERRAFORM_VARS_FILE -force
-  RETURN_CODE=$?
-  until [ $RETURN_CODE -eq "0" ]; do
-    echo "Destroy errored. Retrying!"
-    sleep 5
-    terraform destroy -var-file=$TERRAFORM_VARS_FILE -force
-    RETURN_CODE=$?
-  done
+#  RETURN_CODE=$?
+#  until [ $RETURN_CODE -eq "0" ]; do
+#    echo "Destroy errored. Retrying!"
+#    sleep 5
+#    terraform destroy -var-file=$TERRAFORM_VARS_FILE -force
+#    RETURN_CODE=$?
+#  done
 
   # Delete Azure Active Directory app and Service Principle
   echo "Deleting Azure AD app and Service Principle"
-  az ad app delete --id $(az ad app list | jq -r '.[] | select(.displayName == "Terraform Jumpbox Service Principle") | .appId') 
+  az ad app delete --id $(cat $TERRAFORM_VARS_FILE | grep "client_id" | awk '{print $3}' | tr -d '"')
 
   # Remove the state files. If present, this would take precedence. 
   echo "Deleting $TERRAFORM_DIR/*.tfstate*"
