ceb69fa66269cc8d8fa689608058a143ff4c1427
ecoles-cgi <edgar.coles@cgi.com>
2017-08-30 11:54:17 -0600

    Adding vSphere pipeline options

--- set-pipelines.bat
+++ set-pipelines.bat
@@ -1,1 +1,2 @@
-call ./set-pipeline.bat lite ".\ci\pipeline.yml" ".\ci\env\azure-pipeline-params.yml" ".\credentials.yml" azure-jumpbox
+REM call ./set-pipeline.bat lite ".\ci\pipeline.yml" ".\ci\env\azure-pipeline-params.yml" ".\credentials.yml" azure-jumpbox
+call ./set-pipeline.bat lite ".\ci\pipeline.yml" ".\ci\env\vsphere-pipeline-params.yml" ".\credentials.yml" vsphere-jumpbox
---
+++ vsphere/jumpbox_infra.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+function create_env () {
+  # Create Jumpbox SSH Keypair
+  # ssh-keygen -q -N '' -t rsa -f ~/.ssh/azure-jumpbox -C ubuntu
+  # JUMPBOX_PUBLIC_KEY=$(cat ~/.ssh/azure-jumpbox.pub)
+
+  # Replace place holders
+  cp $TERRAFORM_DIR/terraform.tfvars.example $TERRAFORM_VARS_FILE
+
+  echo "viuser=\"$IAAS_USERNAME\"" >> $TERRAFORM_VARS_FILE
+  echo "vipassword=\"$IAAS_PASSWORD\"" >> $TERRAFORM_VARS_FILE
+
+  # Terraform Apply
+  echo "Running terraform apply"
+  terraform apply -var-file=$TERRAFORM_VARS_FILE
+}
+
+function destroy_env () {
+  # Destroy terraformed jumpbox env 
+  echo "Running terraform destroy"
+  terraform destroy -var-file=$TERRAFORM_VARS_FILE -force
+
+  # Delete Azure Active Directory app and Service Principle
+  echo "Deleting Azure AD app and Service Principle"
+  az ad app delete --id $(cat $TERRAFORM_VARS_FILE | grep "client_id" | awk '{print $3}' | tr -d '"')
+
+  # Remove the state files. If present, this would take precedence. 
+  echo "Deleting $TERRAFORM_DIR/*.tfstate*"
+  rm $TERRAFORM_DIR/*.tfstate*
+
+  # Cleanup Jumpbox SSH keys
+  echo "Removing Jumpbox Key Pair"
+  rm ~/.ssh/azure-jumpbox*
+
+  # Remove terraform vars final
+  echo "Removing terraform vars final"
+  rm $TERRAFORM_VARS_FILE
+}
+
+action=$1
+
+if [ -z $action ]; then
+  echo "Missing argument. Requires one of {apply|destroy|output}"
+  exit 1
+fi
+
+CWD=$(pwd)
+TERRAFORM_DIR=$CWD/terraform
+TERRAFORM_VARS_FILE=$TERRAFORM_DIR/terraform-final.tfvars
+
+cd $TERRAFORM_DIR
+
+if [ $action == output ]; then
+  terraform output -state=$TERRAFORM_DIR/terraform.tfstate
+elif [ $action == apply ]; then
+  create_env
+elif [ $action == destroy ]; then
+  destroy_env
+else
+  echo "Something went wrong!"  
+fi
