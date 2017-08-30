c4a2224de5d6c71fed7cf090544bcb66a6b89dbb
ecoles-cgi <edgar.coles@cgi.com>
2017-08-30 12:54:53 -0600

    Debugging vSphere

--- ci/scripts/create-jumpbox.sh
+++ ci/scripts/create-jumpbox.sh
@@ -6,6 +6,8 @@ cd $IAAS_DIRECTORY
 
 # az login -u $IAAS_USERNAME -p $IAAS_PASSWORD
 
-jumpbox_infra.sh $JUMPBOX_ACTION
+ls -la
 
-jumpbox_infra.sh output | awk '{print $3}' >> jumpbox-keys/ip_address.txt
+./jumpbox_infra.sh $JUMPBOX_ACTION
+
+./jumpbox_infra.sh output | awk '{print $3}' >> jumpbox-keys/ip_address.txt
