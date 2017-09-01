da6ff19b970a292fe3fdac0399c3fbae015f31a8
Michael Minges <mminges@ecsteam.com>
2017-09-01 13:28:34 -0600

    adding verify and ssh functions into script

--- azure/jumpbox_infra.sh
+++ azure/jumpbox_infra.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function create_env () {
+  terraform init
   # Set account level environment variables
   SUBSCRIPTION_ID=$(az account list | jq -r '.[] | select(.isDefault == true) | .id')
   TENANT_ID=$(az account list | jq -r '.[] | select(.isDefault == true) | .tenantId')
@@ -31,6 +32,35 @@ function create_env () {
   terraform apply -var-file=$TERRAFORM_VARS_FILE
 }
 
+function terraform_state_exists () {
+  if [[ ! -f $TERRAFORM_DIR/terraform.tfstate ]]; then
+    echo "terraform.tfstate file does not exist. Have you created the Jumpbox yet?"
+    exit 1
+  fi
+}
+
+function ssh_env () {
+  terraform_state_exists  
+
+  JUMPBOX_IP=$(terraform output -state=$TERRAFORM_DIR/terraform.tfstate --json | jq -r '.jumpbox_public_ip.value')
+  VM_USER=$(cat $TERRAFORM_VARS_FILE | grep "vm_admin_username" | awk '{print $3}' | tr -d '"')
+  ssh -i ~/.ssh/azure-jumpbox -o StrictHostKeyChecking=no $VM_USER@$JUMPBOX_IP
+}
+
+function verify_env () {
+  terraform_state_exists
+
+  JUMPBOX_IP=$(terraform output -state=$TERRAFORM_DIR/terraform.tfstate --json | jq -r '.jumpbox_public_ip.value')
+  echo "exit" | telnet $JUMPBOX_IP 22 | grep "Connected"
+  RETURN_CODE=$(echo -e $?)
+  if [[ $RETURN_CODE == 0 ]]; then
+    echo -e "\nJumpbox is UP!"
+  else
+    echo -e "\nJumpbox is DOWN!"
+    exit 1
+  fi
+}
+
 function destroy_env () {
   # Destroy terraformed jumpbox env 
   echo "Running terraform destroy"
@@ -56,7 +86,7 @@ function destroy_env () {
 action=$1
 
 if [ -z $action ]; then
-  echo "Missing argument. Requires one of {apply|destroy|output}"
+  echo "Missing argument. Requires one of {apply|ssh|verify|destroy}"
   exit 1
 fi
 
@@ -65,15 +95,16 @@ TERRAFORM_DIR=$CWD/terraform
 TERRAFORM_VARS_FILE=$TERRAFORM_DIR/terraform-final.tfvars
 
 cd $TERRAFORM_DIR
-terraform init
 
-az login -u $IAAS_USERNAME -p $IAAS_PASSWORD
+az login -u $IAAS_USERNAME -p $IAAS_PASSWORD 1> /dev/null
 
-if [ $action == output ]; then
-  terraform output -state=$TERRAFORM_DIR/terraform.tfstate
-elif [ $action == apply ]; then
+if [ $action == "apply" ]; then
   create_env
-elif [ $action == destroy ]; then
+elif [ $action == "verify" ]; then
+  verify_env
+elif [ $action == "ssh" ]; then
+  ssh_env
+elif [ $action == "destroy" ]; then
   destroy_env
 else
   echo "Something went wrong!"  
