4eaf6a4723601e42cdde7aa856d7cac814502f38
Michael Minges <mminges@ecsteam.com>
2017-09-01 16:31:13 -0600

    adding terraform init to the destroy task as it is in a new docker container

--- ci/scripts/destroy-jumpbox.sh
+++ ci/scripts/destroy-jumpbox.sh
@@ -4,9 +4,12 @@ set -ex
 
 root_dir=$PWD
 
+cd $IAAS_DIRECTORY/terraform
+# Have to run terraform init again since each task runs in a new docker container
+terraform init
+
+cp $root_dir/jumpbox-artifacts/terraform.tfstate .
+cp $root_dir/jumpbox-artifacts/terraform-final.tfvars .
+
 cd $IAAS_DIRECTORY
-
-cp $root_dir/jumpbox-artifacts/terraform.tfstate terraform/
-cp $root_dir/jumpbox-artifacts/terraform-final.tfvars terraform/
-
 ./jumpbox_infra.sh $JUMPBOX_ACTION
