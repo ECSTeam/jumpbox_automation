cb362896f9b18c4a9356ce970daa94ee0a486244
Michael Minges <mminges@ecsteam.com>
2017-09-12 10:38:45 -0600

    refactoring aws jumpbox_infra script to be similar to the azure jumpbox infra script

--- aws/jumpbox_infra.sh
+++ aws/jumpbox_infra.sh
@@ -1,19 +1,61 @@
 #!/bin/bash
 
+function create_env () {
+  terraform init
+  echo "Running terraform apply"
+  terraform apply -var-file=$TERRAFORM_VARS_FILE
+}
+
+function terraform_state_exists () {
+  if [[ ! -f $TERRAFORM_DIR/terraform.tfstate ]]; then
+    echo "terraform.tfstate file does not exist. Have you created the Jumpbox yet?"
+    exit 1
+  fi
+}
+
+function verify_env () {
+  terraform_state_exists
+
+    JUMPBOX_IP=$(terraform output -state=$TERRAFORM_DIR/terraform.tfstate --json | jq -r '.jumpbox_public_ip.value')
+  echo "exit" | telnet $JUMPBOX_IP 22 | grep "Connected"
+  RETURN_CODE=$(echo -e $?)
+  if [[ $RETURN_CODE == 0 ]]; then
+    echo -e "\nJumpbox is UP!"
+  else
+    echo -e "\nJumpbox is DOWN!"
+    exit 1
+  fi
+}
+
+function ssh_env () {
+  terraform_state_exists
+
+  JUMPBOX_IP=$(terraform output -state=$TERRAFORM_DIR/terraform.tfstate --json | jq -r '.jumpbox_public_ip.value')
+  SSH_KEYNAME=$(cat $TERRAFORM_DIR/terraform.tfvars | grep "aws_key_name" | awk '{print $3}' | tr -d '"')
+  ssh -i ~/.ssh/$SSH_KEYNAME.pem -o StrictHostKeyChecking=no ubuntu@$JUMPBOX_IP
+}
+
 action=$1
 
 if [ -z $action ]; then
-  echo "Missing argument. Requires one of {apply|destroy|output}"
+  echo "Missing argument. Requires one of {apply|verify|ssh|destroy}"
   exit 1
 fi
 
 CWD=$(pwd)
+TERRAFORM_DIR=$CWD/terraform
+TERRAFORM_VARS_FILE=$TERRAFORM_DIR/terraform.tfvars
 
-cd $CWD/terraform
+cd $TERRAFORM_DIR
 
-if [ $action == output ]; then
-  terraform $action -state=terraform.tfstate
+if [ $action == "apply" ]; then
+  create_env
+elif [ $action == "verify" ]; then
+  verify_env
+elif [ $action == "ssh" ]; then
+  ssh_env
+elif [ $action == "destroy" ]; then
+  destroy_env
 else
-  terraform plan -var-file=terraform.tfvars
-  terraform $action -var-file=terraform.tfvars
+  echo "Something went wrong!"
 fi
