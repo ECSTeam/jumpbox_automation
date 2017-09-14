462bf28b7a80cc11b1cab989d6da2b76911f285d
ecoles-cgi <edgar.coles@cgi.com>
2017-09-14 11:10:08 -0600

    Updating base set-pipelines script to contain all potential pipelines

--- set-pipelines.sh
+++ set-pipelines.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-./set-pipeline.sh lite "./ci/pipeline.yml" "./ci/env/azure-pipeline-params.yml" "./credentials.yml" azure-jumpbox
+# ./set-pipeline.sh lite "./ci/pipeline.yml" "./ci/env/azure-pipeline-params.yml" "./credentials.yml" azure-jumpbox
+# ./set-pipeline.sh lite "./ci/pipeline.yml" "./ci/env/vsphere-pipeline-params.yml" "./credentials.yml" vsphere-jumpbox
+# ./set-pipeline.sh lite "./ci/pipeline.yml" "./ci/env/aws-pipeline-params.yml" "./credentials.yml" aws-jumpbox
+# ./set-pipeline.sh lite "./ci/pipeline.yml" "./ci/env/gcp-pipeline-params.yml" "./credentials.yml" gcp-jumpbox
