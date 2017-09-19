55559783b62de00a3e9ba9439ecf0e3234b8b9a1
Ramene Anthony <github@jadedgeek.com>
2017-09-19 09:52:14 -0400

    updating outputs.tf to output jumpbox_public_ip

--- gcp/terraform/outputs.tf
+++ gcp/terraform/outputs.tf
@@ -2,6 +2,10 @@ output "ops_manager_public_ip" {
   value = "${google_compute_instance.jumpbox.network_interface.0.access_config.0.assigned_nat_ip}"
 }
 
+output "jumpbox_public_ip" {
+  value = "${google_compute_instance.jumpbox-public-ip.ip_address}"
+}
+
 output "project" {
   value = "${var.project}"
 }
