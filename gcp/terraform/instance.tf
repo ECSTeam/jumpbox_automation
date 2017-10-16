2bdb121b909485306748ac887817d2f582501696
Steve Wall <swall@ecsteam.com>
2017-10-16 09:22:08 -0600

    trying to get disk configuration working

--- gcp/terraform/instance.tf
+++ gcp/terraform/instance.tf
@@ -15,8 +15,7 @@ resource "google_compute_instance" "jumpbox" {
   tags           = ["${var.prefix}-jumpbox", "jumpbox"]
 
   boot_disk {
-    image = "${var.jumpbox-image-url}"
-    size  = 150
+    source_image = "${var.jumpbox-image-url}"
   }
 
   network_interface {
