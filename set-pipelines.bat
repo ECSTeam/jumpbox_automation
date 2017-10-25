025ace3e0f686546422cffb0232945e462d8ef84
Steve Wall <swall@ecsteam.com>
2017-10-25 10:42:22 -0600

    adding aws

--- set-pipelines.bat
+++
@@ -1,2 +0,0 @@
-REM call ./set-pipeline.bat lite ".\ci\pipeline.yml" ".\ci\env\azure-pipeline-params.yml" azure-jumpbox
-call ./set-pipeline.bat lite ".\ci\pipeline.yml" ".\ci\env\vsphere-pipeline-params.yml"  vsphere-jumpbox
