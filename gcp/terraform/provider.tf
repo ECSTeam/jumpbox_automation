b82b1c5abfa8c2273b34e762d4dcf1926c54914e
Steve Wall <swall@ecsteam.com>
2017-10-30 15:08:39 -0600

    pinned google provider and template provider

--- gcp/terraform/provider.tf
+++ gcp/terraform/provider.tf
@@ -2,4 +2,9 @@ provider "google" {
   credentials = "${file("${var.credentials-file}")}"
   project     = "${var.project}"
   region      = "${var.region}"
+  version     = "~> 1.1"
+}
+
+provider "template" {
+  version = "~> 1.0"
 }
