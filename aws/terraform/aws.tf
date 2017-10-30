c33d927fd8928f2edbe8342dadab33d2f31b3dca
Steve Wall <swall@ecsteam.com>
2017-10-30 15:05:30 -0600

    Pinned versions for aws provider and template provider

--- aws/terraform/aws.tf
+++ aws/terraform/aws.tf
@@ -1,5 +1,10 @@
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
+    region     = "${var.aws_region}"
+    version    = "~> 1.1"
+}
+
+provider "template" {
+    version = "~> 1.0"
 }
