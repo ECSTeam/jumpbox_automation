505ec831c5aa6f9e25ebd55b3f2a16a3c2cb1919
Steve Wall <swall@ecsteam.com>
2017-12-14 14:14:50 -0700

    Converted back to getting aws private key from an environment variable.

--- aws/jumpbox_infra.sh
+++ aws/jumpbox_infra.sh
@@ -26,7 +26,7 @@ function create_env () {
 
   mkdir -p $TERRAFORM_DIR/ssh-key
 
-  cp ~/.ssh/$AWS_KEY_NAME.pem $TERRAFORM_DIR/ssh-key/$AWS_KEY_NAME.pem
+  echo "$AWS_PRIVATE_KEY" >  $TERRAFORM_DIR/ssh-key/$AWS_KEY_NAME.pem
   chmod 0400 $TERRAFORM_DIR/ssh-key/$AWS_KEY_NAME.pem
 
   terraform apply -var-file=$TERRAFORM_VARS_FILE
