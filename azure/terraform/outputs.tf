6242548e41ec931eff43e48f061ebb6f8aa42565
Michael Minges <mingesm@hotmail.com>
2018-01-12 12:40:25 -0700

    adding additional terraform outputs to make it easier to grab values from output to use as vars in bosh create-env

--- azure/terraform/outputs.tf
+++ azure/terraform/outputs.tf
@@ -1,3 +1,21 @@
 output "jumpbox_public_ip" {
   value = "${azurerm_public_ip.jumpbox-public-ip.ip_address}"
 }
+output "internal_cidr" {
+  value = "${var.azure_terraform_subnet_infra_cidr}"
+}
+output "vnet_name" {
+  value = "${azurerm_virtual_network.oss_bosh_infra_virtual_network.name}"
+}
+output "subnet_name" {
+  value = "${azurerm_subnet.oss_bosh_infra_subnet.name}"
+}
+output "resource_group_name" {
+  value = "${azurerm_resource_group.oss_bosh_infra_resource_group.name}"
+}
+output "storage_account_name" {
+  value = "${azurerm_storage_account.bosh_storage_account.name}"
+}
+output "security_group_name" {
+  value = "${azurerm_network_security_group.jumpbox_security_group.name}"
+}
