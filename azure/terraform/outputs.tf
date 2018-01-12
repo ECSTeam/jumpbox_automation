408d61932a13ce62d66965da982dc94a3e32c387
Michael Minges <mingesm@hotmail.com>
2018-01-12 14:05:08 -0700

    adding additional terraform outputs to Azure's outputs.tf

--- azure/terraform/outputs.tf
+++ azure/terraform/outputs.tf
@@ -1,8 +1,11 @@
+output "env_name" {
+  value = "${var.env_name}"
+}
 output "jumpbox_public_ip" {
   value = "${azurerm_public_ip.jumpbox-public-ip.ip_address}"
 }
 output "internal_cidr" {
-  value = "${var.azure_terraform_subnet_infra_cidr}"
+  value = "${azurerm_subnet.oss_bosh_infra_subnet.address_prefix}"
 }
 output "vnet_name" {
   value = "${azurerm_virtual_network.oss_bosh_infra_virtual_network.name}"
@@ -18,4 +21,7 @@ output "storage_account_name" {
 }
 output "security_group_name" {
   value = "${azurerm_network_security_group.jumpbox_security_group.name}"
+}
+output "internal_gw" {
+  value = "${cidrhost(azurerm_subnet.oss_bosh_infra_subnet.address_prefix, 1)}"
 }
